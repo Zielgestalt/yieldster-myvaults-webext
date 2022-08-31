91feb3bac7714128b78a73b24230db7a3e39593a
Sebastian Galleitner <info@zielgestalt.de>
2022-08-31 21:18:38 +0200

    Initital commit yieldster myvaults webext

--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,56 +1,81 @@
 <script setup lang="ts">
-import { darkMode, storageDemo } from '~/logic/storage'
-
-const mode = computed(() => {
-  if (darkMode.value)
-    return 'dark'
-  else
-    return ''
-})
-</script>
-
-<template>
-  <main :data-theme="mode">
-    <div class="container text-flow is-flow-6">
-      <div class="is-centered">
-        <Logo />
-      </div>
-      <p>
-        This is the options page
-      </p>
-
-      <label for="storageDemo">
-        Label for Storage Demo:
-        <input id="storageDemo" v-model="storageDemo" name="storageDemo">
-      </label>
-
-      <div class="toggle">
-        <p>Dark Mode:</p>
-        <label class="switch">
-          <input v-model="darkMode" type="checkbox">
-          <span class="slider round" />
+  import { computedAsync } from '@vueuse/core'
+  import { api, darkMode, hasWallet, staging, vaults, wallet } from '~/logic/storage'
+  
+  console.log('hasWallet: ', hasWallet())
+  
+  const downloads = computedAsync(async (onCancel) => {
+    const abortController = new AbortController()
+  
+    onCancel(() => abortController.abort())
+  
+    return await fetch(
+      `https://api.npmjs.org/downloads/point/last-week/${packageName.value}`,
+      { signal: abortController.signal },
+    )
+      .then(response => response.ok ? response.json() : { downloads: '—' })
+      .then(result => result.downloads)
+  }, 0)
+  
+  const mode = computed(() => {
+    if (darkMode.value)
+      return 'dark'
+    else
+      return ''
+  })
+  </script>
+  
+  <template>
+    <main :data-theme="mode">
+      <div class="container text-flow is-flow-6">
+        <div class="is-centered">
+          <Logo />
+        </div>
+        <!-- <div>Yieldster Extension options page</div> -->
+        <label for="wallet">
+          Your wallet address:
+          <input id="wallet" v-model="wallet" name="wallet">
         </label>
+        <!-- <label for="vaults">
+          Vault address:
+          <input id="vaults" v-model="vaults[0]" name="vaults">
+        </label>
+        <label for="api">
+          API:
+          <input id="api" v-model="api" name="api">
+        </label> -->
+  
+        <div class="toggle">
+          <p>Dark Mode:</p>
+          <label class="switch">
+            <input v-model="darkMode" type="checkbox">
+            <span class="slider round" />
+          </label>
+        </div>
+  
+        <!-- <div class="toggle">
+          <p>Staging:</p>
+          <label class="switch">
+            <input v-model="staging" type="checkbox">
+            <span class="slider round" />
+          </label>
+        </div> -->
       </div>
-
-      <div class="mt-4">
-        Powered by Vite <pixelarticons-zap class="align-middle" />
-      </div>
-    </div>
-  </main>
-</template>
-
-<style scoped lang="scss">
+    </main>
+  </template>
+  
+  <style scoped lang="scss">
   label, fieldset legend {
     display: block;
     margin-bottom: calc(var(--size-5) * .25);
     font-weight: var(--fw-4);
   }
-
+  
   .toggle {
     display: grid;
     grid-template-columns: 1fr 4fr;
   }
-
+  
   /* The switch - the box around the slider */
   .switch {
     position: relative;
@@ -58,14 +83,14 @@ const mode = computed(() => {
     width: 2.5rem;
     height: calc(1rem + 2px);
   }
-
+  
   /* Hide default HTML checkbox */
   .switch input {
     opacity: 0;
     width: 0;
     height: 0;
   }
-
+  
   /* The slider */
   .slider {
     position: absolute;
@@ -78,7 +103,7 @@ const mode = computed(() => {
     -webkit-transition: .4s;
     transition: .4s;
   }
-
+  
   .slider:before {
     position: absolute;
     content: "";
@@ -90,25 +115,26 @@ const mode = computed(() => {
     -webkit-transition: .4s;
     transition: .4s;
   }
-
+  
   input:checked + .slider {
     background-color: #2196F3;
   }
-
+  
   input:focus + .slider {
     box-shadow: 0 0 1px #2196F3;
   }
-
+  
   input:checked + .slider:before {
     transform: translateX(1.25rem);
   }
-
+  
   /* Rounded sliders */
   .slider.round {
     border-radius: 34px;
   }
-
+  
   .slider.round:before {
     border-radius: 50%;
   }
   </style>
+  
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,42 +1,57 @@
 <script setup lang="ts">
-import { darkMode, storageDemo } from '~/logic/storage'
-
-const mode = computed(() => {
-  if (darkMode.value)
-    return 'dark'
-  else
-    return ''
-})
-
-function openOptionsPage() {
-  browser.runtime.openOptionsPage()
-}
-</script>
-
-<template>
-  <main :data-theme="mode">
-    <div class="container text-flow">
-      <div class="is-centered">
-        <Logo />
-      </div>
-
-      <div>Popup</div>
-      <p class="mt-2 opacity-50">
-        This is the popup page
-      </p>
-      <button class="btn mt-2" @click="openOptionsPage">
-        Open Options
-      </button>
-      <div class="mt-2">
-        <span class="opacity-50">Storage:</span> {{ storageDemo }}
-      </div>
-    </div>
-  </main>
-</template>
-
-<style scoped lang="scss">
-  main {
-    min-height: 600px;
-    width: 100%;
+  import { darkMode, hasWallet } from '~/logic/storage'
+  
+  const mode = computed(() => {
+    if (darkMode.value)
+      return 'dark'
+    else
+      return ''
+  })
+  
+  const timekey = Math.floor(Date.now() / 1000)
+  
+  function openOptionsPage() {
+    browser.runtime.openOptionsPage()
   }
-</style>
+  </script>
+  
+  <template>
+    <main :data-theme="mode">
+      <div class="container text-flow">
+        <div class="is-centered">
+          <Logo />
+        </div>
+        <div v-if="hasWallet()">
+          <Suspense>
+            <PopupDisplay :key="timekey" />
+          </Suspense>
+        </div>
+        <div v-else class="no-wallet text-flow">
+          <p>
+            Please enter a wallet address in the options page
+          </p>
+          <button class="btn mt-2" @click="openOptionsPage">
+            Open Options
+          </button>
+        </div>
+      </div>
+    </main>
+  </template>
+  
+  <style scoped lang="scss">
+    main {
+      min-height: 600px;
+      width: 100%;
+    }
+  
+    .no-wallet {
+      padding: 2rem;
+      text-align: center;
+      min-height: 300px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
+  
